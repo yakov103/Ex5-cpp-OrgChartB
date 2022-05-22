201c89298aa16d9ef948cf6a33479cbc7d4ebdca
yakov103 <yaakov103@gmail.com>
2022-05-22 01:30:47 -0700

    100

---
+++ sources/OrgChart.cpp
@@ -0,0 +1,292 @@
+#include "OrgChart.hpp"
+#include <vector>
+namespace ariel {
+
+
+    /* OrgChart Implementation */
+
+    /*
+    *constractor
+    */
+    OrgChart::OrgChart() {
+        _root = nullptr;
+
+    }
+    /*
+    * destructor
+    */
+
+    OrgChart::~OrgChart() {
+       
+    //    while (!_orderedVec.empty()) {
+    //         delete _orderedVec.back();
+    //         _orderedVec.pop_back();
+    //     }
+        
+}
+
+
+
+    OrgChart& OrgChart::operator= (const OrgChart& other) {
+        if (this != &other) {
+            delete _root;
+            _root = other._root;
+        }
+        return *this;
+    }
+
+    /*
+    * add root , if the root is not null just change the name
+    * else create a new root
+    */
+    OrgChart& OrgChart::add_root(const string& name) {
+        if (_root != nullptr) {
+            _root->name = name;
+            return *this;
+        }
+        _root = new Node;
+        _root->name = name;
+        _root->level = 0 ; 
+        return *this;
+    }
+
+    bool OrgChart::check_parent(string parent, string child, Node *node)
+    {
+        if (node->name.compare(parent) == 0)
+        {
+            Node *new_sub = new Node;
+            new_sub->name = child;
+            new_sub->level = node->level +1 ; 
+            node->children.push_back(new_sub);
+            return true;
+        }
+        for (unsigned int i = 0; i < node->children.size(); i++)
+        {
+            //printf("%s\n", node->children[i]->name.c_str());
+            if (check_parent(parent, child, node->children.at(i)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    OrgChart& OrgChart::add_sub(string parent, string child) {
+        if (_root == nullptr) {
+            throw "no root";
+        }
+        if (!check_parent(parent, child, _root))
+        {
+            throw std::invalid_argument("parent not found");
+        }
+        return *this;
+    }
+
+
+    OrgChart::Iterator OrgChart::begin_level_order()  {
+        if (_root == nullptr) {
+            throw "no root";
+        }
+        if (!_orderedVec.empty()){
+            delete _orderedVec.back(); 
+        }
+        _orderedVec.clear();
+        this->_orderedVec.push_back(_root);
+        this->fill_order(_root,LEVEL_ORDER);
+        Node *temp = new Node;
+        temp->name= "ENDITERATOR";
+        this->_orderedVec.push_back(temp);
+        Iterator it = Iterator(&this->_orderedVec, LEVEL_ORDER);
+        return it;
+    }
+
+    /*
+  * This function is used the reverse order.
+  * using this https://stackoverflow.com/questions/48251254/how-can-i-insert-element-into-beginning-of-vector
+  * @return - the end of the reverse order.
+  */
+
+    OrgChart::Iterator OrgChart::begin_reverse_order()  {
+              if (_root == nullptr) {
+            throw "no root";
+        }
+        if (!_orderedVec.empty()){
+            delete _orderedVec.back(); 
+        }
+        this->_orderedVec.clear();
+        Node *temp = new Node;
+        temp->name= "ENDITERATOR";
+        this->_orderedVec.insert(_orderedVec.begin(),temp);
+        //this->_orderedVec.insert(_orderedVec.begin(),_root);
+        this->fill_order(_root,REVERSE_ORDER);
+        Iterator it = Iterator(&this->_orderedVec, REVERSE_ORDER);
+        return it;
+    }
+   OrgChart::Iterator OrgChart::begin_preorder()  {
+        if (_root == nullptr) {
+            throw "no root";
+        }
+        if (!_orderedVec.empty()){
+            delete _orderedVec.back(); 
+        }
+        _orderedVec.clear();
+        this->_orderedVec.push_back(_root);
+        this->fill_order(_root,PRE_ORDER);
+        Node *temp = new Node;
+        temp->name= "ENDITERATOR";
+        this->_orderedVec.push_back(temp);
+        Iterator it = Iterator(&this->_orderedVec, PRE_ORDER);
+        return it;
+   }
+    OrgChart::Iterator OrgChart::end_level_order() {
+              if (_root == nullptr) {
+            throw "no root";
+        }
+        return Iterator(&_orderedVec, END_LEVEL_ORDER);
+    }
+    OrgChart::Iterator OrgChart::reverse_order() {
+              if (_root == nullptr) {
+            throw "no root";
+        }
+        return Iterator(&_orderedVec, END_LEVEL_ORDER);
+    }
+   OrgChart::Iterator OrgChart::end_preorder(){
+             if (_root == nullptr) {
+            throw "no root";
+        }
+          return Iterator(&_orderedVec, END_LEVEL_ORDER);
+   }
+
+    OrgChart::Iterator OrgChart::begin(){
+        return begin_level_order();
+    }
+    OrgChart::Iterator OrgChart::end(){
+        return end_level_order();
+    }
+
+    ostream& operator<<(ostream& os,  OrgChart& org) {
+        org.begin();
+        unsigned int  i ;
+        unsigned int currLevel = 0 ; 
+        unsigned int total = org._orderedVec.size();
+
+        for (i = 0 ; i < org._orderedVec.size()-1; i++){ 
+            if (currLevel  == org._orderedVec.at(i)->level){
+                os << org._orderedVec.at(i)->name << " "; 
+            }
+            else {
+                os << "\n"; 
+                currLevel++; 
+                os << org._orderedVec.at(i)->name << " "; 
+            }
+        }
+       return os;
+    }
+
+
+        // will fill the vector with order that wanted .
+    void OrgChart::fill_order(Node *node, iterator_type type) {
+            unsigned int i = 0; // index for loop
+            if (type == LEVEL_ORDER) {
+                for (i = 0; i < node->children.size(); i++) {
+                    _orderedVec.push_back(node->children.at(i));
+                }
+                for (i = 0; i < node->children.size(); i++) {
+                    fill_order(node->children.at(i), type);
+                }
+                // for (i = 0 ; i < _orderedVec.size() ; i ++){
+                //     cout << _orderedVec.at(i)->name << " ";
+                // }
+                // cout << endl;
+            }
+            else if (type == REVERSE_ORDER){
+
+                queue<Node*> q;
+                q.push(node);
+                while (!q.empty()) {
+                    Node *temp = q.front();
+                    q.pop();
+                    for (i = temp->children.size()-1 ; i >= 0 && i < 4000000000 ; i--) {
+                        q.push(temp->children.at(i));
+                    }
+                    _orderedVec.insert(_orderedVec.begin(), temp);
+                }
+                // for (i = node->children.size()-1 ; i >= 0 && i < 4000000000 ; i--){ // 4000000000 because we are using unsigned int
+                //     _orderedVec.insert(_orderedVec.begin(),node->children.at(i));
+                // }
+
+                // for (i = node->children.size()-1 ; i >= 0 && i < 4000000000 ; i--){ // 4000000000 because we are using unsigned int
+                //     fill_order(node->children.at(i), type);
+                // }
+
+
+            }
+            else if (type == PRE_ORDER){
+                for (i = 0 ; i < node->children.size() ; i++){
+                    _orderedVec.push_back(node->children.at(i));
+                    if (node->children.at(i)->children.size() > 0){
+                        fill_order(node->children.at(i), type);
+                    }
+                }
+            }
+        }
+
+
+
+    /* implementation of the iterator */
+
+
+    OrgChart::Iterator::Iterator(vector<Node*> * nodes, iterator_type type) {
+        _index = 0;
+   
+        _orderedVecIter = nodes;
+        // for (unsigned int i = 0 ; i < _orderedVecIter->size() ; i++){
+        //     cout << _orderedVecIter->at(i)->name << " ";
+        // }
+        if (type == END_LEVEL_ORDER){
+            _index = _orderedVecIter->size()-1;
+        }
+    }
+
+
+    OrgChart::Iterator OrgChart::Iterator::operator++() {
+        this->_index++;
+        return *this;
+    }
+
+
+    OrgChart::Iterator OrgChart::Iterator::operator++(int) {
+        this->_index++;
+        return *this;
+    }
+
+    bool OrgChart::Iterator::operator==(const Iterator& other) {
+        return _orderedVecIter->at(_index)->name == other._orderedVecIter->at(other._index)->name;
+    }
+
+    bool OrgChart::Iterator::operator!=(const Iterator& other) {
+       // printf("%s and %s\n", _orderedVecIter->at(_index)->name.c_str(), other._orderedVecIter->at(_index)->name.c_str());
+        return !(_orderedVecIter->at(_index)->name == other._orderedVecIter->at(other._index)->name);
+    }
+
+    string & OrgChart::Iterator::operator*() {
+        return _orderedVecIter->at(_index)->name;
+    }
+
+    string* OrgChart::Iterator::operator->() {
+        return &_orderedVecIter->at(_index)->name;
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ sources/OrgChart.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <queue>
+#include <stdexcept>
+
+using namespace std;
+
+namespace ariel {
+    enum iterator_type {
+        LEVEL_ORDER,
+        REVERSE_ORDER,
+        PRE_ORDER,
+        END_LEVEL_ORDER,
+        END_REVERSE_ORDER,
+        END_PRE_ORDER
+    };
+
+    struct Node {
+        string name;
+        vector<Node*> children;
+        unsigned int level; 
+    };
+
+    class OrgChart {
+        class Iterator{
+        private :
+
+            unsigned int _index;
+            Node * ptr;
+            vector<Node*> *  _orderedVecIter;
+
+            unsigned int _size{};
+        public :
+            Iterator(vector<Node*> * nodes, iterator_type type);
+            Iterator operator++();
+            Iterator operator++(int);
+            bool operator==(const Iterator& other);
+            bool operator!=(const Iterator& other);
+            string& operator*();
+            string* operator->();
+
+        };
+
+
+
+    public :
+        Node* _root;
+        vector<Node*>  _orderedVec;
+
+        OrgChart();
+        ~OrgChart();
+        void fill_order(Node *node , iterator_type type);
+
+
+        OrgChart& operator= (const OrgChart& other);
+        OrgChart& add_root(const string& name);
+        OrgChart& add_sub(string parent,string child);
+        bool check_parent(string parent, string child , Node *node);
+
+        OrgChart::Iterator begin() ;
+        OrgChart::Iterator end();
+        OrgChart::Iterator begin_level_order() ;
+        OrgChart::Iterator begin_reverse_order() ;
+        OrgChart::Iterator begin_preorder() ;
+        OrgChart::Iterator end_level_order() ;
+        OrgChart::Iterator reverse_order()  ;
+       OrgChart::Iterator end_preorder() ;
+
+
+
+
+        friend ostream& operator<<(ostream& os,OrgChart& org);
+
+
+
+    };
+};
